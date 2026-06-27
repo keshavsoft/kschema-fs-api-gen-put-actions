3d22b2fa918854d98c2b7c45b52b9edae55f6d3b
keshavsoft <nknnkumar@yahoo.com>
2026-06-27 14:55:46 +0530

    rest

--- test/v2/modify/end-points.js
+++ test/v2/modify/end-points.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import funcFrommodify from './modify/controller.js';
+
 const tableName = "StockItems";
 const tablePath = "Data/StockItems.json";
 const configPath = "Config/Schemas/StockItems.json";
 
 const router = express.Router();
+
+router.put('/modify', express.json(), (req, res) => funcFrommodify({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
 
 export { router };
